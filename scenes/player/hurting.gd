7bb68d2dd4305918696e8f7260b0e8def97fa704
Fhil Joshua <husphil200@gmail.com>
2024-11-16 20:09:06 +0800

    added controls

--- scenes/player/hurting.gd
+++ scenes/player/hurting.gd
@@ -2,14 +2,14 @@ extends PlayerState
 
 var attack : Attack 
 
+
 func enter(previous_state_path: String, data := {}) -> void:
-	
 	attack = data.get("attack") as Attack
+	pass
+
+func physics_update(delta : float):
 	if attack:
 		player.state_label.text = "dmg: " + str(attack.atk_damage)
-		print(attack.atk_damage)  # Access the attack object directly and print its atk_damage
-	if player:
-		player.state = player.States.HURTING
-		print(player.NAME)
-		print(previous_state_path)
+	
 	pass
+		
