88db264dd64d5f402ebd9a7c10c5e2c8b83cf534
ZAAALJLJ <tigersharkz314@gmail.com>
2024-11-17 19:44:58 +0800

    added m_ball

--- components/hitbox/detect_player.gd
+++ components/hitbox/detect_player.gd
@@ -7,8 +7,13 @@ func _ready():
 			flippable_sprite.is_flipped.connect(child._on_sprite_flipped)
 			
 
-
+@onready var m_ball = preload("res://scenes/enemy/mangkukulam/m_attack.tscn")
 func _on_body_entered(body: Node2D) -> void:
 	if body == get_parent().player:
+		var k = m_ball.instantiate()
+		k.dir = rotation
+		k.spawnPos = global_position
+		k.spawnRot = rotation
+		get_parent().add_child(k)
 		print("MAY PLAYER")
 	pass # Replace with function body.
