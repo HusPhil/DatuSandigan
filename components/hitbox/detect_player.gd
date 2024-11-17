18957ef6936a564ac236e2cac9799614699d5be4
ZAAALJLJ <tigersharkz314@gmail.com>
2024-11-17 21:20:40 +0800

    specified attack

--- components/hitbox/detect_player.gd
+++ components/hitbox/detect_player.gd
@@ -10,10 +10,12 @@ func _ready():
 @onready var m_ball = preload("res://scenes/enemy/mangkukulam/m_attack.tscn")
 func _on_body_entered(body: Node2D) -> void:
 	if body == get_parent().player:
-		var k = m_ball.instantiate()
-		k.dir = rotation
-		k.spawnPos = global_position
-		k.spawnRot = rotation
-		get_parent().add_child(k)
+		var path = get_parent().name
+		if path == "Mangkukulam":
+			var k = m_ball.instantiate()
+			k.dir = rotation
+			k.spawnPos = global_position
+			k.spawnRot = rotation
+			get_parent().add_child(k)
 		print("MAY PLAYER")
 	pass # Replace with function body.
